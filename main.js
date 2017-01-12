9195b3db2484ed23c38777e0642a8ecb8faa8e30
Aliya Bhatia <bhatia.aliya@gmail.com>
2017-01-12 14:33:12 -0500

    click to change firefox icon

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hi, I'm a JavaScript file")
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/firefox-icon.png'){
+		myImage.setAttribute ('src','images/firefox-icon-snazzy.png');
+	}
+	else {
+		myImage.setAttribute ('src','images/firefox-icon.png');
+	}
+}
