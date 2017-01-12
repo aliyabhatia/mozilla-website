a1845f51520b74bf64d89a1b512ebda9224aa547
Aliya Bhatia <bhatia.aliya@gmail.com>
2017-01-12 15:04:26 -0500

    2 different ways to trigger an action via queries

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-document.querySelector('h1').onclick = function() {
+// Section for header alert functions
+// One way to do this is to have the function defined seperately from the query selector
+
+function HeadingAlert() {
 	alert('dont touch the headers!!');
 }
+
+
+// Section for query selectors
+
+// QuerySelector1 --> clicking the headings
+// note that you did not includ () after HeaderAlert!!
+document.querySelector('h1').onclick = HeadingAlert
+
+// QuerySelector2 --> clicking the image
+// Note that you can trigger the function immediately without having it separately - compere to above
 
 var myImage = document.querySelector('img');
 
